99325b88ef2abe750c3e58cdbd11b0886afb23ea
Georgi Ditsov <g.ditsov@proxiad.com>
2021-01-03 20:58:39 +0200

    Refactoring the code in weatherService

--- service/weatherService.js
+++ service/weatherService.js
@@ -2,30 +2,22 @@ import { API_KEY } from "../utils/weather/weather-api-key";
 
 const BASE_URL = `http://api.openweathermap.org/data/2.5/`;
 
-export const fetchWeatherForCurrentLocation = async (latitude, longitude) => {
-  const result = await fetch(
+export const fetchWeatherForCurrentLocation = (latitude, longitude) =>
+  fetch(
     `${BASE_URL}weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
-  );
-  return await result.json();
-};
+  ).then((response) => response.json());
 
-export const fetchWeatherForCity = async (city) => {
-  const result = await fetch(
+export const fetchWeatherForCity = (city) =>
+  fetch(
     `${BASE_URL}weather?q=${city}&APPID=${API_KEY}&units=metric`
-  );
-  return await result.json();
-};
+  ).then((response) => response.json());
 
-export const fetchForecastforCurrentLocation = async (latitude, longitude) => {
-  const result = await fetch(
+export const fetchForecastforCurrentLocation = (latitude, longitude) =>
+  fetch(
     `${BASE_URL}forecast?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
-  );
-  return await result.json();
-};
+  ).then((response) => response.json());
 
-export const fetchForecastForCity = async (city) => {
-  const result = await fetch(
+export const fetchForecastForCity = (city) =>
+  fetch(
     `${BASE_URL}forecast?q=${city}&APPID=${API_KEY}&units=metric`
-  );
-  return await result.json();
-};
+  ).then((response) => response.json());
